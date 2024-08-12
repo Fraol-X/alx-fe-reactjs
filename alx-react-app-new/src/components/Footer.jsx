dedd9b96a0cd0c6a0728930144ad591162e7fb04
Fraol-X <pfresh641@gmail.com>
2024-08-12 10:26:02 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+    return (
+        <footer style={ { backgroundColor: 'brown', color: 'white', fontSize: '12px', textAlign: 'center' } }>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+
+}
+
+export default Footer;
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent () {
+    return (
+        <main style={ { backgroundColor: 'black', color: 'lightBlue', padding: '10px', fontSize: '15px' } }>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>);
+}
+
+export default MainContent;
