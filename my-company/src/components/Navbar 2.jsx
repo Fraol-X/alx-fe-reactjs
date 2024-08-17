dda339d522412e5c4de8ef14fa33f7c3c8ef5302
Fraol-X <pfresh641@gmail.com>
2024-08-17 13:40:03 +0300

    Build a Simple Company Website with React

---
+++ my-company/src/components/Navbar 2.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+function Navbar () {
+    return (
+        <nav style={{ backgroundColor: 'lightgray', paddingTop: '30px', textAlign: 'right'}}>
+            <ul>
+                <Link to='/home' style={{paddingRight: '50px', color: 'green'}}>Home</Link>
+                <Link to='/about' style={{paddingRight: '50px', color: 'green'}}>About</Link>
+                <Link to='/services' style={{paddingRight: '50px', color: 'green'}}>Services</Link>
+                <Link to='/contact' style={{paddingRight: '100px', color: 'green'}}>Contact</Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
