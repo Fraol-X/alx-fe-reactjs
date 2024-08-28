1705c2db9a859cc0d83747efb8bc633b042cb4ea
Fraol-X <pfresh641@gmail.com>
2024-08-28 09:56:50 +0300

    Advanced Routing in React with React Router

---
+++ react-router-advanced/src/components/PostsComponent.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useQuery} from 'react-query'
+
+const fetchPosts = async () => {
+    const response = await fetch ('https://jsonplaceholder.typicode.com/posts');
+    return response.json();
+};
+
+const PostsComponent = () => {
+    const {data, error, isError, isLoading, refetch} = useQuery('fetchPosts', fetchPosts, {
+        staleTime: 10000,
+        cacheTime: 60000,
+        keepPreviousData: true,
+        refetchOnWindowFocus: true
+    });
+
+    if (isLoading) return <div>Loading...</div>
+    if(isError) return <div>Error loading data: {error.message}</div>
+
+    return (
+    <div>
+        {data.map(item => (
+            <div key={item.id}>{item.name}</div>
+        ))}
+        <button onClick={()=> refetch}>Refetch Data</button>
+    </div>
+    )
+}
+
+export default PostsComponent
