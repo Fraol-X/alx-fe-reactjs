a86fb6ae4a53352402cf0e2680684447736f4ba9
Fraol-X <pfresh641@gmail.com>
2024-09-19 11:36:42 +0300

    tool ' from Looks like we cant find the user

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
       const response = await fetchUserData(query);
       setUser(response.data); 
     } catch (err) {
-      setError('Looks like we can’t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false); 
     }
